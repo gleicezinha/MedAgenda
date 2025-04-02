24d104982ac26cd61778781b99c07b81f1c06e90
Dayan Freitas Alves <contatodayanfa@gmail.com>
2025-04-02 09:56:44 -0500

    Delete medagendaapp directory

--- medagendaapp/src/app/components/i-crud-form.ts
+++
@@ -1,4 +0,0 @@
-export interface ICrudForm<T> {
-    registro: T;
-    save(): void;
-}
--- medagendaapp/src/app/interceptors/auth.interceptor.ts
+++
@@ -1,7 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
-
-
-export const autenticacaoInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
