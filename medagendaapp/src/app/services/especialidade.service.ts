42c2c6cd598074be6255c509e7f26e6c6ccffb04
JohnPabloTW0 <john.rocha@sou.ufac.br>
2025-04-06 21:21:47 -0500

    Dayan tu vai consertar isso

--- medagendaapp/src/app/services/especialidade.service.ts
+++ medagendaapp/src/app/services/especialidade.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Especialidade } from '../models/especialidade.model';
+import { ICrudService } from './i-crud-service';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EspecialidadeService {
+export class EspecialidadeService implements ICrudService<Especialidade> {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+  apiUrl: string = environment.API_URL + '/especialidade';
+
+  get(termoBusca?: string): Observable<Especialidade[]> {
+    throw new Error('Method not implemented.');
+  }
+
+  getById(id: number): Observable<Especialidade> {
+    throw new Error('Method not implemented.');
+  }
+
+  save(objeto: Especialidade): Observable<Especialidade> {
+    throw new Error('Method not implemented.');
+  }
+  
+  delete(id: number): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
 }
