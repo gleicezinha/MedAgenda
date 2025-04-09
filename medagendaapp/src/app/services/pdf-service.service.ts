37f626e31d5fc86ceec7233bc10013e6a717cd44
JohnPabloTW0 <john.rocha@sou.ufac.br>
2025-04-08 22:08:32 -0500

    feat: baixar pdf no front

--- medagendaapp/src/app/services/pdf-service.service.ts
+++ medagendaapp/src/app/services/pdf-service.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PdfServiceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  apiUrl: string = environment.API_URL 
+
+  atestado(): Observable<Blob>{
+    let url = this.apiUrl + "/atestado/pdf";
+    return this.http.get(url, {responseType: 'blob'})
+  }
+
+  pericia(): Observable<Blob>{
+    let url = this.apiUrl + "/pericia/pdf";
+    return this.http.get(url, {responseType: 'blob'})
+  }
+
+  exame(): Observable<Blob>{
+    let url = this.apiUrl + "/exame/pdf";
+    return this.http.get(url, {responseType: 'blob'})
+  }
 }
